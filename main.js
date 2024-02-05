9a25cf4dd3d967bc74acae227957a0a3a11541d3
fageer <fageern@gmail.com>
2024-02-05 21:11:37 +0300

    some changes

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function getPrayerTimingsOfCity(cityName){
         city: cityName //"Ar Riyāḑ"
     };
     
-    axios.get('http://api.aladhan.com/v1/timingsByCity', {
+    axios.get('https://api.aladhan.com/v1/timingsByCity', {
         params: params
       })
       .then(function (response) {
